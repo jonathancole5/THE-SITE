267648f2d78267f2f979d899b8eb5e92e81f55b4
jonathancole5 <37849318+jonathancole5@users.noreply.github.com>
2019-03-11 12:08:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function peepee1(el){
+	if (document.getElementById("win").style.visibility = "visible"){
+	
+	}
+	else if (document.getElementById("win").style.visibility = "hidden") {
+		document.getElementById("one").play();
+		document.getElementById("win").style.visibility = "visible";
+	}
+}
+
+function peepee2(el){
+	document.getElementById("win").style.visibility = "hidden";
+}
+
+function ha(el){
+	window.alert("I am 𝘯𝘰𝘵 looking in there.");
+}
