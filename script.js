7e1871b3ca7f8457d6b911b6c572efb26619180c
jonathancole5 <37849318+jonathancole5@users.noreply.github.com>
2019-03-12 12:52:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+window.onload = setTimeout(message, 100);
+
+var ba = 0;
+var mo = 0;
+var bt = 0;
+var win = 0;
+
+function message(){
+	alert("Man, this guy seems to like classical music... Wait a minute.")
+}
+
 function peepee1(el){
 	if (document.getElementById("win").style.visibility = "visible"){
 	
 	}
-	else if (document.getElementById("win").style.visibility = "hidden") {
+	else if (document.getElementById("win").style.visibility = "hidden"){
 		document.getElementById("one").play();
 		document.getElementById("win").style.visibility = "visible";
 	}
@@ -14,4 +25,40 @@ function peepee2(el){
 
 function ha(el){
 	window.alert("I am 𝘯𝘰𝘵 looking in there.");
+}
+/* Hey Jonathan, this is Trevor. 
+You used setInteval, which will loop the function indefinitely.
+I would advice you use setTimeout instead, as it is only used once. Not entirely sure what you wanted
+to do, but I thought I would just say that.
+*/
+function bach(){
+	ba = 1;
+	checkwin();
+	console.log(ba, mo, bt, win)
+}
+
+function motz(){
+	mo = 1;
+	checkwin();
+}
+
+function beet(){
+	bt = 1;
+	checkwin();
+}
+
+function other(){
+	ba = 0;
+	mo = 0;
+	bt = 0;
+	peepee1();
+}
+
+function checkwin(){	
+	if ((ba == 1) && (mo == 1) && (bt == 1)){
+		window.alert("Yay");
+	}
+	else {
+		
+	}
 }
